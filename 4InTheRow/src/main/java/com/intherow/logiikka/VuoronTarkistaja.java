edee32e0f1472b21627c586f16287bb59de2e375
Jusaa <jussiliu@JUSSIN-KONE.home>
2016-10-21 17:06:17 +0300

    Dokumentaatiopäivitys

--- 4InTheRow/src/main/java/com/intherow/logiikka/VuoronTarkistaja.java
+++ 4InTheRow/src/main/java/com/intherow/logiikka/VuoronTarkistaja.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.intherow.logiikka;
 
 import com.intherow.ui.Piirtaja;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- *
- * @author Jusaa
- */
 public class VuoronTarkistaja {
 
     private Piirtaja piirtaja;
